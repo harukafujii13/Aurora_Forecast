08622487dc99b165636e06139a61c85e8762d061
Haruka <hfujii1013@gmail.com>
2023-08-08 19:17:34 -0700

    style: change blur

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import DisplayKp from './components/DisplayKp';
 function App() {
   return (
     <div className="min-h-screen relative">
-      <div className="bg-[url('./assets/main-background.jpg')] bg-cover min-h-screen absolute w-full h-full blur-sm"></div>
+      <div className="bg-[url('./assets/main-background.jpg')] bg-cover min-h-screen absolute w-full h-full blur-[5px]"></div>
       <div className="bg-text relative">
         <Header />
         <SearchForm />
